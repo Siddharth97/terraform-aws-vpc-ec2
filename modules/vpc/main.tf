6a49d0a1180be4c9245a1b6219bb876c2fdbbf5f
Siddharth Mehra <siddharth.mehra@tulip.com>
2025-08-29 13:52:04 -0400

    add internet gateway to expose public subnet to internet, add add public and private route tables with associations

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,6 +8,14 @@ resource "aws_vpc" "this" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
 resource "aws_subnet" "public" {
   for_each = { for idx, az in var.azs : idx => az }
   vpc_id                  = aws_vpc.this.id
@@ -29,4 +37,39 @@ resource "aws_subnet" "private" {
   tags = {
     Name = "private-subnet-${each.key + 1}"
   }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route" "igw_route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private route table (NO internet route)
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  for_each       = aws_subnet.private
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
 }
